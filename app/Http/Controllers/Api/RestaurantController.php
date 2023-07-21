8cf58ae4f70b4675c81e8c33cf54ac7c004dd1ee
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-07-21 14:01:38 +0200

    Condition in Restaurant Controller Api

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -8,8 +8,23 @@ use App\Models\Admin\Restaurant;
 
 class RestaurantController extends Controller
 {
-    public function index(){
-        $restaurants = Restaurant::all();
+    public function index(Request $request){
+
+        $query = Restaurant::with(['types']); 
+
+        if($request->has('type_id')){
+            // con explode da 2,4,5 diventa [2,4,5]
+            $type_id = explode(',', $request->type_id);
+
+            // type_id =[2,4,5]
+
+            // il primo parametro whereHas è il nome del metodo di relazione tra le tabelle restaurants e types scritta nel model restaurant.php
+            $query->whereHas('types', function($query)use($type_id){
+                $query->whereIn('id', $type_id);
+            });
+        }
+
+        $restaurants = $query->get();
 
         return response()->json([
             'success'=> true,
