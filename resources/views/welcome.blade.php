81b94b9e13a85f59677f075dd41cf3bc0a750a56
RiccardoMartella <riccardomartella77@gmail.com>
2023-07-24 10:16:13 +0200

    add style register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<div class="bg-food vh-100">
-    <h1 class="text-center">Login</h1>
+<div class="bg-food div-food">
+  <div class="img-login" >
+  </div>
+  <div class="d-flex w-50 m-auto mt-4 justify-content-around ">
+    <div class="div-register ms-5">Login</div>
+    <div class="div-register me-5">Register</div>
+  </div>
 </div>
+<div class="div-color "></div>
 @endsection
