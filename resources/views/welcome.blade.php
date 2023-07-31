d789ed3f2f809bb24dab59690d4e56109fa6e353
andreamastro98 <andreamastrovito98@gmail.com>
2023-07-31 16:13:12 +0200

    button modificati

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
 <div class="bg-food div-food d-flex justify-content-center align-items-center px-5">
   {{-- <div class="img-login"></div> --}}
   <div class="d-flex justify-content-around align-items-center">
-    <div class="mx-2  div-register">
-      <a class="text-decoration-none" href="{{ route('login') }}">{{ __('Login') }}
+    <div class="mx-4  div-register">
+      <a class="text-decoration-none ui-btn" href="{{ route('login') }}">{{ __('Login') }}
       </a>
     </div>
-    <div class="mx-2  div-register">
-      <a class="text-decoration-none" href="{{ route('register') }}">{{ __('Register') }}</a>
+    <div class="mx-4  div-register">
+      <a class="text-decoration-none ui-btn" href="{{ route('register') }}">{{ __('Register') }}</a>
     </div>
   </div>
   <div class="div-color "></div>
